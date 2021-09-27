d8adafd7c04dfb047656518feedbf94ad714ac4d
ImranShirzad <iks9320@live.co.uk>
2021-09-28 00:10:56 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿
+namespace BMIsystem
+    {
+    partial class Form1
+        {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+            {
+            if (disposing && (components != null))
+                {
+                components.Dispose();
+                }
+            base.Dispose(disposing);
+            }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+            {
+            this.weightLbl = new System.Windows.Forms.Label();
+            this.heightLbl = new System.Windows.Forms.Label();
+            this.resultLbl = new System.Windows.Forms.Label();
+            this.txtWeight = new System.Windows.Forms.TextBox();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.btnCal = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.radBtnKg = new System.Windows.Forms.RadioButton();
+            this.radBtnPound = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // weightLbl
+            // 
+            this.weightLbl.AutoSize = true;
+            this.weightLbl.Location = new System.Drawing.Point(84, 61);
+            this.weightLbl.Name = "weightLbl";
+            this.weightLbl.Size = new System.Drawing.Size(41, 13);
+            this.weightLbl.TabIndex = 0;
+            this.weightLbl.Text = "Weight";
+            // 
+            // heightLbl
+            // 
+            this.heightLbl.AutoSize = true;
+            this.heightLbl.Location = new System.Drawing.Point(84, 121);
+            this.heightLbl.Name = "heightLbl";
+            this.heightLbl.Size = new System.Drawing.Size(38, 13);
+            this.heightLbl.TabIndex = 1;
+            this.heightLbl.Text = "Height";
+            // 
+            // resultLbl
+            // 
+            this.resultLbl.AutoSize = true;
+            this.resultLbl.Location = new System.Drawing.Point(577, 37);
+            this.resultLbl.Name = "resultLbl";
+            this.resultLbl.Size = new System.Drawing.Size(37, 13);
+            this.resultLbl.TabIndex = 2;
+            this.resultLbl.Text = "Result";
+            // 
+            // txtWeight
+            // 
+            this.txtWeight.Location = new System.Drawing.Point(131, 58);
+            this.txtWeight.Name = "txtWeight";
+            this.txtWeight.Size = new System.Drawing.Size(240, 20);
+            this.txtWeight.TabIndex = 3;
+            this.txtWeight.TextChanged += new System.EventHandler(this.txtWeight_TextChanged);
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(131, 121);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(240, 20);
+            this.txtHeight.TabIndex = 4;
+            // 
+            // btnCal
+            // 
+            this.btnCal.Location = new System.Drawing.Point(131, 184);
+            this.btnCal.Name = "btnCal";
+            this.btnCal.Size = new System.Drawing.Size(240, 23);
+            this.btnCal.TabIndex = 5;
+            this.btnCal.Text = "Calculate";
+            this.btnCal.UseVisualStyleBackColor = true;
+            this.btnCal.Click += new System.EventHandler(this.btnCal_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(580, 61);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(289, 130);
+            this.txtResult.TabIndex = 6;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(580, 225);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 7;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(794, 225);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 8;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // radBtnKg
+            // 
+            this.radBtnKg.AutoSize = true;
+            this.radBtnKg.Location = new System.Drawing.Point(431, 87);
+            this.radBtnKg.Name = "radBtnKg";
+            this.radBtnKg.Size = new System.Drawing.Size(65, 17);
+            this.radBtnKg.TabIndex = 9;
+            this.radBtnKg.TabStop = true;
+            this.radBtnKg.Text = "Kilogram";
+            this.radBtnKg.UseVisualStyleBackColor = true;
+            // 
+            // radBtnPound
+            // 
+            this.radBtnPound.AutoSize = true;
+            this.radBtnPound.Location = new System.Drawing.Point(431, 148);
+            this.radBtnPound.Name = "radBtnPound";
+            this.radBtnPound.Size = new System.Drawing.Size(56, 17);
+            this.radBtnPound.TabIndex = 10;
+            this.radBtnPound.TabStop = true;
+            this.radBtnPound.Text = "Pound";
+            this.radBtnPound.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(924, 301);
+            this.Controls.Add(this.radBtnPound);
+            this.Controls.Add(this.radBtnKg);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnCal);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.txtWeight);
+            this.Controls.Add(this.resultLbl);
+            this.Controls.Add(this.heightLbl);
+            this.Controls.Add(this.weightLbl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+            }
+
+        #endregion
+        private System.Windows.Forms.Label weightLbl;
+        private System.Windows.Forms.Label heightLbl;
+        private System.Windows.Forms.Label resultLbl;
+        private System.Windows.Forms.TextBox txtWeight;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.Button btnCal;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.RadioButton radBtnKg;
+        private System.Windows.Forms.RadioButton radBtnPound;
+        }
+    }
+
---
+++ Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BMIsystem
+    {
+    public partial class Form1 : Form
+        {
+        public Form1()
+            {
+            InitializeComponent();
+            }
+
+        
+
+        private void txtWeight_TextChanged(object sender, EventArgs e)
+            {
+
+            }
+
+        private void btnExit_Click(object sender, EventArgs e)
+            {
+            this.Close();
+            }
+
+        private void btnReset_Click(object sender, EventArgs e)
+            {
+            txtWeight.Text = "";
+            txtHeight.Text = "";
+            txtResult.Text = "";
+
+            radBtnKg.Checked = false;
+            radBtnPound.Checked = false;
+            }
+
+        private void btnCal_Click(object sender, EventArgs e)
+            {
+            double weight = Convert.ToDouble(txtWeight.Text);
+            double height = Convert.ToDouble(txtHeight.Text);
+            double result = 0.0;
+            string result2 = "";
+
+            if (radBtnKg.Checked)
+                {
+                weight = weight / (height * height);
+                result = weight;
+                }
+            else
+            if (radBtnPound.Checked)
+                {
+                weight = weight / 2.205;
+                result = weight / (height * height);
+                }
+
+            if (result < 18.5)
+                {
+                result2 = "Your'e in the underweight range";
+                }
+            else
+             if (result > 18.5)
+                {
+                result2 = "Your'e in the healthy weight range";
+                }
+            else
+             if (result > 29.9)
+                {
+                result2 = "Your'e in the overweight range";
+                }
+            else
+             if (result > 39.9)
+                {
+                result2 = "Your'e in the obese range";
+                }
+                
+                
+            txtResult.Text = "Your BMI is " + Math.Round(result, 5) +"\r\n"+ result2;
+             }
+        }
+    }
